96e9bb0b3b1a3c4b3261625b6e38a45756572d87
jairahrodriguez <jairahrodriguez@mymail.mapua.edu.ph>
2025-10-25 22:00:22 +0800

    Update update_profile.php

--- CultureLens_API/endpoints/update_profile.php
+++ CultureLens_API/endpoints/update_profile.php
@@ -1,5 +1,9 @@
 <?php
 header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
 include("../config/db_connect.php");
 include("../utils/auth.php");
 
@@ -57,3 +61,4 @@ if ($stmt->execute()) {
 
 $conn->close();
 ?>
+
