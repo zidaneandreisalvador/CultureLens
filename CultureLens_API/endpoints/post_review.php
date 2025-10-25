06dff9f25efcbd0cf56b1f83aaa22a2f7ec1e323
jairahrodriguez <jairahrodriguez@mymail.mapua.edu.ph>
2025-10-25 21:59:43 +0800

    Update post_review.php

--- CultureLens_API/endpoints/post_review.php
+++ CultureLens_API/endpoints/post_review.php
@@ -1,5 +1,9 @@
 <?php
 header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
 include("../config/db_connect.php");
 include("../utils/auth.php");
 
@@ -40,3 +44,4 @@ if ($stmt->execute()) {
 
 $conn->close();
 ?>
+
