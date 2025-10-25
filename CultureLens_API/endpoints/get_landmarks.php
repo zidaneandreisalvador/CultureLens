86810927432a897b7dfd489aed8b2e38a0e43013
jairahrodriguez <jairahrodriguez@mymail.mapua.edu.ph>
2025-10-25 21:59:07 +0800

    Update get_landmarks.php

--- CultureLens_API/endpoints/get_landmarks.php
+++ CultureLens_API/endpoints/get_landmarks.php
@@ -1,5 +1,9 @@
 <?php
 header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
 include("../config/db_connect.php");
 
 $countryID = $_GET["country_id"] ?? null;
@@ -33,3 +37,4 @@ echo json_encode([
 
 $conn->close();
 ?>
+
