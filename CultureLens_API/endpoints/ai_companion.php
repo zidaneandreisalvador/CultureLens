2d5930241d91d62821de4e52a0d4488ba32ad644
jairahrodriguez <jairahrodriguez@mymail.mapua.edu.ph>
2025-10-25 21:57:59 +0800

    Update ai_companion.php

--- CultureLens_API/endpoints/ai_companion.php
+++ CultureLens_API/endpoints/ai_companion.php
@@ -1,5 +1,9 @@
 <?php
 header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
 include("../config/db_connect.php");
 include("../utils/auth.php");
 
@@ -48,3 +52,4 @@ if (isset($ai["error"])) {
 
 echo json_encode(["success"=>true,"reply"=>$answer]);
 ?>
+
