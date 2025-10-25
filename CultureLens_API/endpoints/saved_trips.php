0548709ec89e05eed209e886bc1b5d7ae3244603
jairahrodriguez <jairahrodriguez@mymail.mapua.edu.ph>
2025-10-25 22:00:03 +0800

    Update saved_trips.php

--- CultureLens_API/endpoints/saved_trips.php
+++ CultureLens_API/endpoints/saved_trips.php
@@ -1,5 +1,9 @@
 <?php
 header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
 include("../config/db_connect.php");
 include("../utils/auth.php");
 
@@ -78,3 +82,4 @@ elseif ($method === "GET") {
 
 $conn->close();
 ?>
+
