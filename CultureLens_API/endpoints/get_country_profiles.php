25c0d3875244e94eca06087c7bc174f5eb20ce26
jairahrodriguez <jairahrodriguez@mymail.mapua.edu.ph>
2025-10-25 21:58:33 +0800

    Update get_country_profiles.php

--- CultureLens_API/endpoints/get_country_profiles.php
+++ CultureLens_API/endpoints/get_country_profiles.php
@@ -1,5 +1,9 @@
 <?php
 header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
 include("../config/db_connect.php");
 include("../utils/auth.php");
 
@@ -42,3 +46,4 @@ echo json_encode([
 
 $conn->close();
 ?>
+
