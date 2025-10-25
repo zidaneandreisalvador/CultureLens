77f3e08adc02800972b7250eddce755c649b7e04
jairahrodriguez <jairahrodriguez@mymail.mapua.edu.ph>
2025-10-25 21:58:56 +0800

    Update get_info.php

--- CultureLens_API/endpoints/get_info.php
+++ CultureLens_API/endpoints/get_info.php
@@ -1,5 +1,9 @@
 <?php
 header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
 include("../config/db_connect.php");
 
 // Optional: ?type=about / contact / faq
@@ -56,3 +60,4 @@ echo json_encode([
 
 $conn->close();
 ?>
+
