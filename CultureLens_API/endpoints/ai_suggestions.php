1a41fd28f726e513b253cbc8bdae1c734538ed73
jairahrodriguez <jairahrodriguez@mymail.mapua.edu.ph>
2025-10-25 21:58:24 +0800

    Update ai_suggestions.php

--- CultureLens_API/endpoints/ai_suggestions.php
+++ CultureLens_API/endpoints/ai_suggestions.php
@@ -1,5 +1,9 @@
 <?php
 header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
 include("../config/db_connect.php");
 include("../utils/auth.php");
 
@@ -72,3 +76,4 @@ echo json_encode([
 
 $conn->close();
 ?>
+
